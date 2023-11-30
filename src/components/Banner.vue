c75cd2ee9f1f6d4edf0fed1c10568bf5a504c6de
jjmrbldz <jejo.baldoza@gmail.com>
2023-11-30 15:33:06 +0800

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,45 @@
+<template>
+     <section class="slide-sec">
+        <Carousel class="slide" :autoplay="2000" :wrap-around="true">
+            <Slide v-for="slide in banners" :key="slide">
+                <img :src="slide.imgURL" />
+            </Slide>
+            <template #addons>
+                <Navigation />
+                <Pagination />
+            </template>
+        </Carousel>
+    </section>
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+export default {
+    name: 'Banner',
+    data() {
+        return {
+            banners: [
+                {
+                    imgURL: "src/assets/img/bgs/banner-1.jpg"
+                },
+                {
+                    imgURL: "src/assets/img/bgs/banner-2.jpg"
+                },
+                {
+                    imgURL: "src/assets/img/bgs/banner-3.jpg"
+                }
+            ],
+        }
+    },
+    components: {
+        Carousel,
+        Slide,
+        Navigation
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template lang="">
+    <header>
+        <div class="top">
+            <div class="container">
+                <span class="date" id="dateAndTime">13:15 11-15-2023 GMT +8:00</span>
+                <div class="btns">
+                    <button class="btn btn-gold">로그인</button>
+                    <button class="btn btn-gold">회원가입</button>
+                </div> 
+            </div>
+        </div>
+        <div class="bottom">
+            <button class="menu-toggle">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div class="navbar-logo animate__animated  animate__fadeIn">
+                <img src="@/assets/img/navbar-logo.png" alt="">
+            </div>
+            <div class="container">
+                <div class="nav left">
+                    <a href="#">
+                        <span>라이브카지노</span>
+                        <span>live casino</span>
+                    </a>
+                    <a href="#">
+                        <span>슬롯게임</span>
+                        <span>slot games</span>
+                    </a>
+                    <a href="#">
+                        <span>마이페이지</span>
+                        <span>my page</span>
+                    </a>
+                    <a href="#">
+                        <span>고객센터</span>
+                        <span>customer</span>
+                    </a>
+                </div>
+                <div class="nav right">
+                    <a href="#">
+                        <span>공지사항</span>
+                        <span>notice</span>
+                    </a>
+                    <a href="#">
+                        <span>충전신청</span>
+                        <span>deposit</span>
+                    </a>
+                    <a href="#">
+                        <span>환전신청</span>
+                        <span>withdraw</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
